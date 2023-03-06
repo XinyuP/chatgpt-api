dca4fa56a4a3c3a989434aa2e0f49258a3a497da
XinyuP <xinyup@umich.edu>
2023-03-05 23:59:51 -0500

    ChatGPT + Nodejs Server

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ import { Configuration, OpenAIApi } from 'openai';
 import { createRequire } from 'module';
 
 const require = createRequire(import.meta.url);
+const express = require('express');
+const app = express();
+const port = 1337;
+
+app.use(express.json());
+app.use(require('cors')());
+
 require('dotenv').config();
 
 const openAI_SECRET_KEY = process.env.OPENAI_SECRET_KEY;
@@ -11,37 +18,36 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-async function sendPrompt() {
+async function sendPrompt(input) {
 	const model = 'gpt-3.5-turbo';
 	const messages = [
 		{
 			role: 'system',
-			content: 'You are a helpful assistant',
-        },
-        {
-			role: 'user',
-			content: 'How to make banana muffins?',
+			content: 'You are a helpful assistant.',
 		},
-    ];
-    
-    const completion = await openai.createChatCompletion({
-        model,
-        messages
-    })
-    console.log(completion.data.choices)
+		{
+			role: 'user',
+			content: input,
+		},
+	];
+
+	const completion = await openai.createChatCompletion({
+		model,
+		messages,
+	});
+	console.log(completion.data.choices);
+	return completion.data.choices;
 }
 
+// sendPrompt();
 
+// route
+app.post('/api', async (req, res) => {
+	const { prompt } = req.body;
+	const answer = await sendPrompt(prompt);
+	res.status(200).json({
+		message: answer,
+	});
+});
 
-
-
-sendPrompt()
-
-
-
-
-
-
-
-
-
+app.listen(port, () => console.log(`Server has started on port: ${port}`));
